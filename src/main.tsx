165e3dba8d994621d89936738041ecc7c9e16f34
Nitin <nitin.dhaiya@mastersunion.org>
2025-12-25 22:01:59 +0530

    feat: register service worker for PWA support in main entry file

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { registerServiceWorker } from './utils/pwa';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  registerServiceWorker();
+}
 
 
 
